43234741016169e6cdef4772300170a1e1abdb69
Seth <neaseth@deloitte.com>
2020-10-07 21:16:02 -0400

    adding shift press in addition to mouse movement

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ An easy way to prevent your machine from falling asleep
 
 ## Installation and Usage
 1. Install Python - https://www.python.org/downloads/
+⋅⋅* Be sure to check the 'add to PATH' option when prompted  
 
-2. Install dependencies - run `installer.bat`
 
-3. Start the program - run `start.bat`
+2. Install dependencies by running `installer.bat`
+
+3. Start the program by running `start.bat`
--- move.py
+++ move.py
@@ -4,11 +4,12 @@ import threading
 
 screenWidth, screenHeight = pyautogui.size() 
 
-def triggerMove():
+def triggerAction():
+	targetX = random.randint(0, screenWidth)
 	targetY = random.randint(0, screenHeight)
-	targetX = random.randint(0,screenWidth)
-	print("moving to: ", targetX,targetY)
-	pyautogui.moveTo(targetX, targetY, duration=0.25)
-	threading.Timer(3, triggerMove).start()
+	print("moving to: ", targetX, targetY)
+	pyautogui.moveTo(targetX, targetY, duration=0.3)
+	pyautogui.press("shift")
+	threading.Timer(5, triggerAction).start()
 
-triggerMove();
+triggerAction();
